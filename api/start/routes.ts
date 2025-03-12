c54effa2222f8e714c1472958522ba3eca3ba0a8
sebcriado <contact.sebastiencriado@gmail.com>
2025-03-12 15:26:05 +0100

    refactor(user): rename fullName to firstname and lastname, add phone field feat(auth): implement register and login methods in AuthController chore: update package dependencies

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -8,9 +8,11 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const AuthController = () => import('#controllers/auth_controller')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router
+  .group(() => {
+    router.post('/auth/register', [AuthController, 'register'])
+    router.post('/auth/login', [AuthController, 'login'])
+  })
+  .prefix('/api/v1')
